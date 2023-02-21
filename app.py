a35fee2aa5f82820021769e75fc1dd120ecd91b8
EnricoWebers <74408194+EnricoWebers@users.noreply.github.com>
2023-02-21 12:35:10 +0000

    added jsonify

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  { 
+    'id': 1,
+    'title' : 'Data Analyst',
+    'location': 'Bengaluru',
+    'salary': 'Rs 10,00,000'
+  },
+  { 
+    'id': 2,
+    'title' : 'Data Scientist',
+    'location': 'Delhi',
+    'salary': 'Rs 15,00,000'
+  },
+  { 
+    'id': 3,
+    'title' : 'Front End Engineer',
+    'location': 'Remote',
+  },
+  { 
+    'id': 4,
+    'title' : 'Back End Engineer',
+    'location': 'San Francisco',
+    'salary': 'USD 19,000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", 
+                           jobs=JOBS,
+                           company_name='Enrico')
+  
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
-print(__name__)
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
