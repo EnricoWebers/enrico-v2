d7ddedf93d66e687d26a2864a194fe9e3c1cec78
EnricoWebers <74408194+EnricoWebers@users.noreply.github.com>
2023-03-23 13:52:11 +0000

    Completed database functionality

--- app.py
+++ app.py
@@ -1,42 +1,18 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
-
-JOBS = [
-  { 
-    'id': 1,
-    'title' : 'Data Analyst',
-    'location': 'Bengaluru',
-    'salary': 'Rs 10,000,000'
-  },
-  { 
-    'id': 2,
-    'title' : 'Data Scientist',
-    'location': 'Delhi',
-    'salary': 'Rs 15,000,000'
-  },
-  { 
-    'id': 3,
-    'title' : 'Front End Engineer',
-    'location': 'Sittard',
-  },
-  { 
-    'id': 4,
-    'title' : 'Back End Engineer',
-    'location': 'San Francisco',
-    'salary': 'USD 19,000'
-  }
-]
-
+  
 @app.route("/")
 def hello_world():
+    jobs = load_jobs_from_db()
     return render_template("home.html", 
-                           jobs=JOBS,
+                           jobs=jobs,
                            company_name='Enrico')
   
 @app.route("/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    return jsonify(jobs)
 
 
 if __name__ == "__main__":
---
+++ database.py
@@ -0,0 +1,53 @@
+from sqlalchemy import create_engine, text
+
+db_connection_string = "mysql+pymysql://g49jjg744emluyy3ilul:pscale_pw_MMlzHAqK0tFiVDcfAxBNlQcaYB6k8VVVApkp0NftflK@eu-central.connect.psdb.cloud/enricocareers?charset=utf8mb4"
+
+engine = create_engine(
+  db_connection_string,
+  connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem"
+        }
+    })
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    column_names = result.keys()   
+    jobs = []
+    for row in result.all():
+      jobs.append(dict(zip(column_names,row)))
+    return jobs
+    '''
+    print(result_dicts)
+    '''
+
+
+'''
+print("type(result): ",type(result))
+result_all = result.all()
+print("type(result_all): ", type(result_all))
+print("result_all: ",result_all)
+first_result = result_all[0]
+column_names = result.keys() 
+first_result_dict = dict(zip(column_names, first_result))
+print("type(first_result_dict): ",type(first_result_dict))
+print(first_result_dict)
+'''
+
+'''
+LEGACY CODE
+print("first_result: ", first_result)
+print("type(first_result): ", type(first_result))
+first_result_dict = dict(result_all[0].__dict__)
+print("type(first_result_dict): ",type(first_result_dict))
+print(first_result_dict)
+'''
+
+'''
+https://stackoverflow.com/questions/1958219/how-to-convert-sqlalchemy-row-object-to-a-python-dict
+'''
+ 
+'''
+THIS VERSION WORKS according to the YouTube video https://www.youtube.com/watch?v=yBDHkveJUf4&t=10984s
+'''
