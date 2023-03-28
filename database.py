166caed156ee2b72267fd0d4fa02073024bb6216
EnricoWebers <74408194+EnricoWebers@users.noreply.github.com>
2023-03-28 13:16:02 +0000

    added navigation and job pages

--- database.py
+++ database.py
@@ -24,6 +24,23 @@ def load_jobs_from_db():
     '''
 
 
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"SELECT * FROM jobs WHERE id={id}")
+      )
+    rows = []
+    for row in result.all():
+      rows.append(row._mapping)
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
+      '''
+      the following returns a list of 1 element:
+      return [dict(row) for row in rows]
+      '''
+    
 '''
 print("type(result): ",type(result))
 result_all = result.all()
